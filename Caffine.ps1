662d8aaaebe2b79458bfda73543687684955311b
jkirby87 <jkirby87@live.com>
2018-12-20 09:15:00 -0700

    Create Caffine.ps1

---
+++ Caffine.ps1
@@ -0,0 +1,8 @@
+Function caffine {
+    $wsh = New-Object -ComObject Wscript.Shell
+    WHILE($TRUE){
+        $wsh.SendKeys("{SCROLLLOCK}")
+        $wsh.SendKeys("{SCROLLLOCK}")
+        START-SLEEP -Seconds 200
+    }
+}
